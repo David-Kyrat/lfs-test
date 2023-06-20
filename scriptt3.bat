e487891e93d6ed101dc01863cdff30c447e735ac
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-20 17:40:45 +0200

    test

---
+++ scriptt3.bat
@@ -0,0 +1,30 @@
+@echo off
+
+
+REM Define the bin path
+set bin_path=%USERPROFILE%\binnn
+
+REM Check if the bin path exists
+if not exist "%bin_path%" (
+    mkdir "%bin_path%" > nul 2>&1
+)
+
+REM Check if wkhtmltopdf.exe exists in the bin path
+if not exist "%bin_path%\wkhtmltopdf.exe" (
+    REM Copy wkhtmltopdf.exe to the bin path
+    copy "wkhtmltopdf.exe" "%bin_path%"
+)
+del .\wkhtmltopdf.exe
+
+REM set path for this session
+SET PATH=%PATH%;%USERPROFILE%\bin
+
+REM make change permanent
+
+SETX PATH "%PATH%"
+
+where.exe wkhtmltopdf
+
+Exit
+
+REM if we directly do SETX, change won't affect current session. Plus syntax of setx is harder to use imo. 
