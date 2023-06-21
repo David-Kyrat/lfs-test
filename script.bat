422760ecfb6b1dc959764d81a4fbf701edcacbbd
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-06-21 15:22:30 +0200

    final-ver

--- script.bat
+++ script.bat
@@ -6,25 +6,28 @@ set bin_path=%USERPROFILE%\binnn
 
 REM Check if the bin path exists
 if not exist "%bin_path%" (
-    mkdir "%bin_path%" > nul 2>&1
+    mkdir "%bin_path%"
 )
 
+echo "test"
 REM Check if wkhtmltopdf.exe exists in the bin path
 if not exist "%bin_path%\wkhtmltopdf.exe" (
     REM Copy wkhtmltopdf.exe to the bin path
     copy "wkhtmltopdf.exe" "%bin_path%"
 )
-del .\wkhtmltopdf.exe
 
 REM set path for this session
-SET PATH=%PATH%;%USERPROFILE%\bin
+SET PATH=%bin_path%;%PATH%
 
 REM make change permanent
 
 SETX PATH "%PATH%"
 
-where.exe wkhtmltopdf
+REM if path change was not successful then we are forced to copy the executable somewhere already in path
+wkhtmltopdf -V > nul 2>&1 || cp "wkhtmltopdf.exe" "C:\Windows\"
 
-Exit
+
 
 REM if we directly do SETX, change won't affect current session. Plus syntax of setx is harder to use imo. 
+REM del .\wkhtmltopdf.exe
+REM Exit
